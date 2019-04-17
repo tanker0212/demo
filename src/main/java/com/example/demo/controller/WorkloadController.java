b9c511bb2d94801f29838f4ab0045473434c365d
hyunwoo.lim <tanker0212@gmail.com>
2019-04-17 21:18:23 +0900

    add startWorkload()

--- src/main/java/com/example/demo/controller/WorkloadController.java
+++ src/main/java/com/example/demo/controller/WorkloadController.java
@@ -1,10 +1,14 @@
 package com.example.demo.controller;
 
+import java.io.IOException;
+
 import com.example.demo.service.Workload;
 
+import org.apache.http.client.ClientProtocolException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
@@ -18,8 +22,16 @@ public class WorkloadController {
     
     @GetMapping("/test")
     @ResponseBody
-    public int getRequest() {
-        int reval = workload.setLoad();
-        return reval;
+    public int getRequest(@RequestParam(required = false) Integer loop) {
+        //System.out.println(loop);
+        int retval = workload.setLoad(loop);
+        return retval;
+    }
+
+
+    @GetMapping("/work")
+    @ResponseBody
+    public void loadTheWork(@RequestParam String ip, @RequestParam int repeat, @RequestParam int load) throws ClientProtocolException, IOException {
+        workload.startWorkload(ip, repeat, load);
     }
 }
--- src/main/java/com/example/demo/service/Workload.java
+++ src/main/java/com/example/demo/service/Workload.java
@@ -1,5 +1,14 @@
 package com.example.demo.service;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.springframework.stereotype.Service;
 
 /**
@@ -8,9 +17,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class Workload {
 
-    public int setLoad() {
+    public int setLoad(Integer loop) {
+        if (loop == null)
+            loop = 1024;
+        System.out.println(loop);
         int x = 1;
-        for (int i = 0; i < 4096; i++) {
+        for (int i = 0; i < loop; i++) {
             for (int j = 0; j < i; j++) {
                 for (int k = 0; k < j; k++) {
                     x += i + j + k;
@@ -19,4 +31,52 @@ public class Workload {
         }
         return x;
     }
+
+    public void startWorkload(String ip, int repeat, int load) throws ClientProtocolException, IOException {
+
+        String GET_URL = "http://" + ip + "/test?loop=" + load;
+        long start = System.currentTimeMillis();
+        long tmp = start;
+
+        for (int i = 0; i < repeat; i++) {
+            //http client 생성
+            CloseableHttpClient httpClient = HttpClients.createDefault();
+    
+            //get 메서드와 URL 설정
+            HttpGet httpGet = new HttpGet(GET_URL);
+    
+            //get 요청
+            CloseableHttpResponse httpResponse = httpClient.execute(httpGet);
+            
+            System.out.println("::GET Response Status::");
+            
+            //response의 status 코드 출력
+            System.out.println(httpResponse.getStatusLine().getStatusCode());
+    
+            BufferedReader reader = new BufferedReader(new InputStreamReader(
+                    httpResponse.getEntity().getContent()));
+    
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+    
+            while ((inputLine = reader.readLine()) != null) {
+                response.append(inputLine);
+            }
+            
+            reader.close();
+    
+            //Print result
+
+            System.out.println((System.currentTimeMillis() - tmp)/1000.0);
+            tmp = System.currentTimeMillis();
+            
+            httpClient.close();
+        }
+
+        long end = System.currentTimeMillis();
+        double runningTime = (end - start)/1000.0;
+        System.out.println("실행 시간 : " + runningTime);
+        System.out.println("총 요청 수 : " + repeat);
+    }
+
 }
