58d204fe394e1671a0ae2cfebcf5b434f302ca80
임현우 <44562496+tanker0212@users.noreply.github.com>
2019-06-28 20:09:03 +0900

    Add files via upload

--- src/main/java/com/example/demo/controller/ScalingController.java
+++ src/main/java/com/example/demo/controller/ScalingController.java
@@ -4,6 +4,7 @@ import com.example.demo.service.Monitor;
 import com.example.demo.service.Scaling;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 /**
  * ScalingController
  */
+@Controller
 public class ScalingController {
 
     @Autowired
@@ -37,13 +39,6 @@ public class ScalingController {
     @ResponseBody
     public String getDesiredCPU() {
         return "Target Cpu usage : " + scaling.getDesiredCPU() + "%";
-    }
-
-    @GetMapping("/setTime")
-    @ResponseBody
-    public String setSchedulingTime(@RequestParam int time) {
-        monitor.setSchedulingTime(time);
-        return "Scheduling Time : " + monitor.getSchedulingTime() + " ms";
     }
 
     @GetMapping("/setPodMinMax")
